45b85b1474ae40832b0e9ec4558139fbb0ccdb86
AllaLoginova <122536827+AllaLoginova@users.noreply.github.com>
2025-02-20 19:50:23 +0500

    Add files via upload

---
+++ recipes/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from recipes.models import Category, Recipes
+
+
+class AddPostForm(forms.ModelForm):
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label='Категория не выбрана', label='Категории')
+
+    class Meta:
+        model = Recipes
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat', 'tags']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input'}),
+            'content': forms.Textarea(attrs={'cols': 50, 'rows': 5}),
+        }
+        labels = {'slug': 'URL'}
+
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 20:
+            raise ValidationError("Длина превышает 20 символов")
+
+        return title
+
+class UploadFileForm(forms.Form):
+    file = forms.ImageField(label="Файл")
+
+
